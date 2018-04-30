8b56b1e8b64e7478ab5857e6754d17ff9d1fdad5
ming3juan <ming3juan@gmail.com>
2018-04-30 13:35:43 -0400

    Added scheduling tables, fixed comments

--- PharmDFinderSQLScript.sql
+++ PharmDFinderSQLScript.sql
@@ -1,83 +1,91 @@
 --CREATE DATABASE PharmDFinderDB
-
 GO
+
 -- To DROP old tables
-DROP TABLE [Pharmacy]
-DROP TABLE [Pharmacist]
+ALTER TABLE dbo.AssignedSchedules  
+DROP CONSTRAINT FK_AssignedPharmacy_PharmacyId;
+ALTER TABLE dbo.AssignedSchedules  
+DROP CONSTRAINT FK_AssignedPharmacist_PharmacistId;
+
+DROP TABLE AssignedSchedules
+GO
+
+ALTER TABLE dbo.OpenSchedules  
+DROP CONSTRAINT FK_OpenPharmacy_PharmacyId;
+
+DROP TABLE OpenSchedules
+GO
+
+DROP TABLE Pharmacy
+GO
+
+DROP TABLE Pharmacist
+GO
+
 -- ALTER TABLE [Pharmacy] ADD Test VARCHAR(10) DEFAULT 'aa' NOT NULL
 
 GO
-CREATE TABLE [Pharmacy]
+CREATE TABLE Pharmacy
 (
-	[PharmacyId] SMALLINT CONSTRAINT pk_PharmacyId PRIMARY KEY IDENTITY,
-
-	--@"^([A-Z]{1}[a-z]*)+([\s]{1}[A-Z]{1}[a-z]*)*$"
-	[Name] VARCHAR(100) CONSTRAINT uq_Name UNIQUE NOT NULL,
-
-	--@"^([A-Z]{1}[a-z]*)+([\s]{1}[A-Z]{1}[a-z]*)*$"
-	[DBA] VARCHAR(100) DEFAULT ' ',
-
-	[Address] VARCHAR(100) CONSTRAINT uq_Address NOT NULL,
-	--@"^\d{5,5}$)|(^\d{5,5}-\d{4,4}$/ 
-	-- 5 digits or 5 digits then - then 4 digits
-	[ZipCode] CHAR(5) NOT NULL,
-
-	--@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" 
-	--1 or more letters then @ then 1 or more letters then . then 2-4 letters
-	[EmailId] VARCHAR(100) CONSTRAINT uq_Pharmacy_EmailId UNIQUE NOT NULL,
-
-	-- @"^[\d]{10,10}$" --10 digits
-	[TelephoneNum] CHAR(10) CONSTRAINT uq_Pharmacy_TelephoneNum UNIQUE NOT NULL,
-
-	--@"^[\d]{10,10}$" --10 digits
-	[Fax] CHAR(10) CONSTRAINT uq_Fax UNIQUE,
-
-	--@"^[\d]{7,7}$" --7 digits
-	[NCPDP] CHAR(7) CONSTRAINT uq_NCPDP UNIQUE NOT NULL,
-
-	--@"^[\d]{10,10}$" --10 digits
-	[NPI] CHAR(10) CONSTRAINT uq_NPI UNIQUE NOT NULL,
-
-	--@"^[a-zA-Z]{2,2}\d{7,7}" --2 letters then 7 digits
-	[DEA] CHAR(9) CONSTRAINT uq_DEA UNIQUE NOT NULL,
-
-	--@"^\d{6,6}" --6 digits
-	[LicenseNum] CHAR(6) CONSTRAINT uq_Pharmacy_LicenseNum UNIQUE NOT NULL,
-
-	--@"^\d{9,9}" --9 digits
-	[TaxId] CHAR(9) CONSTRAINT uq_TaxId UNIQUE NOT NULL
+	PharmacyId SMALLINT CONSTRAINT PK_PharmacyId PRIMARY KEY IDENTITY,
+	PharmacyName VARCHAR(100) CONSTRAINT UQ_Name UNIQUE NOT NULL, --@"^([A-Z]{1}[a-z]*)+([\s]{1}[A-Z]{1}[a-z]*)*$"
+	DBA VARCHAR(100) DEFAULT ' ', --@"^([A-Z]{1}[a-z]*)+([\s]{1}[A-Z]{1}[a-z]*)*$"
+	[Address] VARCHAR(100) CONSTRAINT UQ_Address NOT NULL,
+	ZipCode CHAR(5) NOT NULL,--@"^\d{5,5}$)|(^\d{5,5}-\d{4,4}$/ 
+	EmailId VARCHAR(100) CONSTRAINT UQ_PharmacyEmailId UNIQUE NOT NULL,	--@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" 
+	TelephoneNum CHAR(10) CONSTRAINT UQ_PharmacyTelephoneNum UNIQUE NOT NULL,	-- @"^[\d]{10,10}$" --10 digits
+	Fax CHAR(10) CONSTRAINT UQ_Fax UNIQUE,	--10 digits
+	NCPDP CHAR(7) CONSTRAINT UQ_NCPDP UNIQUE NOT NULL,	--7 digits
+	NPI CHAR(10) CONSTRAINT UQ_NPI UNIQUE NOT NULL, --10 digits
+	DEA CHAR(9) CONSTRAINT UQ_DEA UNIQUE NOT NULL, --@"^[a-zA-Z]{2,2}\d{7,7}" --2 letters then 7 digits
+	LicenseNum CHAR(6) CONSTRAINT UQ_PharmacyLicenseNum UNIQUE NOT NULL, --6 digits
+	TaxId CHAR(9) CONSTRAINT UQ_TaxId UNIQUE NOT NULL --9 digits
 )
 
 GO
 
-CREATE TABLE [Pharmacist]
+CREATE TABLE Pharmacist
 (
-	[PharmacistId] SMALLINT CONSTRAINT pk_PharmacistId PRIMARY KEY IDENTITY,
-
-	--@"^[A-Z]{1}[a-zA-Z]+*$"
-	[FirstName] VARCHAR(50) NOT NULL,
-	--@"^[A-Z]{1}[a-zA-Z]+*$"
-	[LastName] VARCHAR(50) NOT NULL,
-
-	--@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" 
-	-- 1 or more letter then @ then 1 or more letter then . then 2-4 letters
-	[EmailId] VARCHAR(100) CONSTRAINT uq_Pharmacist_EmailId UNIQUE NOT NULL,
-
-	--@"^[\d]{10,10}$" --10 digits
-	[TelephoneNum] CHAR(10) CONSTRAINT uq_Pharmacist_TelephoneNum UNIQUE NOT NULL,
-
-	--@"^\d{6,6}" --6 digits
-	[LicenseNum] CHAR(6) CONSTRAINT uq_Pharamcist_LicenseNum UNIQUE NOT NULL,
-
-	--@"^[A-Z]{2}$"
-	[StateOfLicense] CHAR(2) NOT NULL,
-
+	PharmacistId SMALLINT CONSTRAINT PK_PharmacistId PRIMARY KEY IDENTITY,
+	FirstName VARCHAR(50) NOT NULL,--@"^[A-Z]{1}[a-zA-Z]+*$",
+	LastName VARCHAR(50) NOT NULL, --@"^[A-Z]{1}[a-zA-Z]+*$"
+	EmailId VARCHAR(100) CONSTRAINT UQ_PharmacistEmailId UNIQUE NOT NULL, --@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$" 
+	[Address] VARCHAR(100) NOT NULL,
+	ZipCode CHAR(5) NOT NULL, --@"^\d{5,5}$)|(^\d{5,5}-\d{4,4}$/"
+	TelephoneNum CHAR(10) CONSTRAINT UQ_PharmacistTelephoneNum UNIQUE NOT NULL, --@"^[\d]{10,10}$" --10 digits
+	LicenseNum CHAR(6) CONSTRAINT UQ_PharamcistLicenseNum UNIQUE NOT NULL,--6 digits
+	StateOfLicense CHAR(2) NOT NULL, --2 captial letters
 	--GREATER THAN 1950 - less than equal CURRENT YEAR
-	[YearOfLicensure] INT CONSTRAINT chk_YearOfLicensure CHECK(YearOfLicensure > 1980 AND YearOfLicensure <= YEAR(GETDATE()) ) NOT NULL,
+	YearOfLicensure INT CONSTRAINT CHK_YearOfLicensure CHECK (YearOfLicensure <= YEAR(GETDATE()) AND YearOfLicensure > 1950)  NOT NULL,
+	CONSTRAINT UQ_AddressFirstNameLastName UNIQUE ([Address], FirstName, LastName) --Can't have same full name and address
 )
 
 GO
 
-INSERT INTO Pharmacy VALUES ('Dude Pharmacy', DEFAULT, '14 Monroe St', '10002', 'dude@gmail.com', '1234567890', '0987654321', '1', '1', '1', '1', '1')
-INSERT INTO Pharmacy VALUES ('', DEFAULT, '14 Monroe St', '10002', '', '', '', '', '', '2', '2', '2')
+CREATE TABLE AssignedSchedules
+(
+	AssignedScheduleId INT CONSTRAINT PK_AssignedScheduleId PRIMARY KEY IDENTITY,
+	PharmacyId SMALLINT CONSTRAINT FK_AssignedPharmacy_PharmacyId REFERENCES Pharmacy(PharmacyId) NOT NULL,
+	PharmacistId SMALLINT CONSTRAINT FK_AssignedPharmacist_PharmacistId REFERENCES Pharmacist(PharmacistId) NOT NULL,
+	StartDateTime SMALLDATETIME CONSTRAINT CK_AssignedStartDateTime CHECK (StartDateTime > GETDATE()) NOT NULL,
+	EndDateTime SMALLDATETIME CONSTRAINT CK_AssignedEndDateTime CHECK (EndDateTime > GETDATE()) NOT NULL,
+	PayRate DECIMAL(5,2) CONSTRAINT CK_AssignedPayRate CHECK (PayRate > 0) NOT NULL,
+)
+
+GO
+
+CREATE TABLE OpenSchedules
+(
+	OpenScheduleId INT CONSTRAINT PK_OpenScheduleId PRIMARY KEY IDENTITY,
+	PharmacyId SMALLINT CONSTRAINT FK_OpenPharmacy_PharmacyId REFERENCES Pharmacy(PharmacyId) NOT NULL,
+	StartDateTime SMALLDATETIME CONSTRAINT CK_OpenStartDateTime CHECK (StartDateTime > GETDATE()) NOT NULL,
+	EndDateTime SMALLDATETIME CONSTRAINT CK_OpenEndDateTime CHECK (EndDateTime > GETDATE()) NOT NULL,
+	PayRate DECIMAL(5,2) CONSTRAINT CK_OpenPayRate CHECK (PayRate > 0) NOT NULL,
+	IsPayNegotiable CHAR(3) CONSTRAINT CK_IsPayNegotiable_YorN CHECK(IsPayNegotiable IN ('Y', 'N'))
+)
+
+GO
+
+--INSERT INTO Pharmacy VALUES ('Dude Pharmacy', DEFAULT, '14 Monroe St', '10002', 'dude@gmail.com', '1234567890', '0987654321', '1', '1', '1', '1', '1')
+--INSERT INTO Pharmacy VALUES ('', DEFAULT, '14 Monroe St', '10002', '', '', '', '', '', '2', '2', '2')
 --INSERT INTO Pharmacist VALUES('Awesome', 'Juan', 'awesome@gmail.com', '1234567890'
